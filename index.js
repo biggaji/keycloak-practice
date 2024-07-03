be0693a9f55d886313e757b01abd5bdf5510ac2b
biggaji <tobiajibade2017@yandex.com>
2024-07-03 01:48:46 +0100

    (chore): Wrote comments to help with understanding implementations

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ import Keycloak from 'keycloak-connect';
 import { config } from 'dotenv';
 import path from 'node:path';
 
+// Loads environment variables
 config();
 
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 // Setup session middleware
 const memoryStore = new session.MemoryStore();
@@ -31,14 +29,24 @@ const keycloak = new Keycloak(
   path.resolve(process.cwd(), 'keycloak.json'),
 );
 
+// Mount keycloak middleware
 app.use(keycloak.middleware());
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handles request to the index route
 app.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Hello World API',
   });
 });
 
+/**
+ * Handles request to the /pro route
+ * This route is protected by Keycloak.
+ * It requires the requesting client to provide an access token from Keycloak server which is then validated by this middleware keycloak.protect()
+ */
 app.get('/pro', keycloak.protect(), (req, res, next) => {
   res.status(200).json({
     message: 'This route is protected by keycloak',
